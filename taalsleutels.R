a47fdcf5bfe8edb5afe3217708aa4eab7a025265
albartcoster <albart@dairyconsult.nl>
2022-08-29 21:43:50 +0200

    eerste commit van dit repo

---
+++ taalsleutels.R
@@ -0,0 +1,25 @@
+## taalsleutels
+sleutels <- list()
+## datacats
+sleutels$dilcat$nl <- c("Alle DIL","0-30 DIL","31-60 DIL","61-200 DIL","> 200 DIL")
+sleutels$dilcat$de <- c("Alle Lakt.","0-30 MT","31-60 MT","61-200 MT",">200 MT")
+sleutels$nlactcat$nl <- c("Alle lakts.",1,2,"3+")
+sleutels$nlactcat$de <- c("Alle lakts.",1,2,"3+")
+
+## snelzicht
+sleutels$snelzicht <- list()
+sleutels$snelzicht$nl <- "Snelzicht"
+sleutels$tabsnelzicht$nl <- c('aantal','% vaars','dil','productie','BSK','%vet','%eiwit','celgetal')
+sleutels$cptsnelzicht$nl <- "Snelzicht van laatste 18 maaden in systeem."
+
+## grote tabellen
+sleutels$overzichten$nl <- 'Overzichten'
+sleutels$cptproduction$nl <- "Overzicht van gemiddelde productie per melkcontrole, laktatienummer en laktatiestadium."
+sleutels$cptfat$nl <- "Overzicht van gemiddelde vetgehalte per melkcontrole, laktatienummer en laktatiestadium."
+sleutels$cptprotein$nl <- "Overzicht van gemiddelde eiwitgehalte per melkcontrole, laktatienummer en laktatiestadium."
+sleutels$cptlact$nl <- "Overzicht van gemiddelde lactosegehalte per melkcontrole, laktatienummer en laktatiestadium."
+sleutels$cptketose$nl <- "Percentage koeien met Ketoseattentie. Ketoseattentie is (%vet - %eiwit) > 1.5 EN %eiwit < 3.25."
+sleutels$cptacidose$nl <- "Percentage koeien met pensverzuringsattentie. Pensverzuringsattentie is %eiwit > %vet EN %vet < 4."
+sleutels$cptscc$nl <- "Gemiddelde celgetal per melkcontrole, laktatienummer en laktatiestadium."
+sleutels$cptchoog$nl <- "Percentage koeien met verhoogd celgetal per melkcontrole, laktatienummer en laktatiestadium. Verhoging is >150 bij vaarzen en >200 bij oudere koeien."
+
